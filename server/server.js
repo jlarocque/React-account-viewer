2ab81a66640215862404deab783b474d5c7fe360
Jeff LaRocque <j.larocque001@gmail.com>
2020-05-04 16:53:49 -0400

    React set up
    -changed file structure to be more react friendly
    -configured webpack/babel

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
+const sqlite3 = require('sqlite3');
 const express = require('express')
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3');
 const PORT = process.env.PORT || 4000;
+const path = require('path');
+const dbPath = path.resolve(__dirname, '../db/accounts.db')
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
 
 const app = express();
+
 app.use(bodyParser.json());
 
 app.get('/', function(req,res){
-  res.send("server is working");
+  db.all('SELECT * FROM accounts', (err, data) =>{
+    if (err) {
+      console.log(err)
+      res.sendStatus(404)
+    } else {
+      res.send(data)
+    }
+  })
 });
 
 app.listen(PORT, function(){
   console.log(`App running on localhost:${PORT}`);
 });
+
